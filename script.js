7108ee7b8ebf4d1aabbd1f96ea2394c99bb4f154
Gopi2923 <dgopidhikshan@gmail.com>
2024-05-24 12:48:46 +0530

    Added validation messages.

--- script.js
+++ script.js
@@ -16,6 +16,66 @@ function validateName() {
         nameError.innerHTML = "Write full name"
         return false;
     }
-    nameError.innerHTML = 'valid';
+    nameError.innerHTML = '<i class="fa-solid fa-circle-check"></i>';
     return true;
+}
+
+function validatePhone() {
+    let phone = document.getElementById('contact-phone').value;
+
+    if(phone.length == 0) {
+        phoneError.innerHTML = "Phone number is required"
+        return false;
+    }
+    if(phone.length != 10) {
+        phoneError.innerHTML = "Phone no should be 10 digits"
+        return flase;
+    }
+    if(!phone.match(/^[0-9]{10}$/)) {
+        phoneError.innerHTML = "Enter only numbers";
+        return flase;
+    }
+    phoneError.innerHTML = '<i class="fa-solid fa-circle-check"></i>';
+    return true;
+}
+
+function validateEmail() {
+    let email = document.getElementById('contact-email').value;
+
+    if(email.length == 0) {
+        emailError.innerHTML = "Email id is required"
+        return false;
+    }
+    if(!email.match(/^[A-Za-z\._\-[0-9]*[@][A-Za-z]*[\.][a-z]{2,4}$/)) {
+        emailError.innerHTML = "Email Invalid"
+        return false;
+    }
+
+
+    emailError.innerHTML = '<i class="fa-solid fa-circle-check"></i>';
+    return true;
+}
+
+function validateMessage() {
+    let message = document.getElementById('contact-message').value;
+    let required = 30;
+    let left = required - message.length; 
+
+    if(left > 0) {
+        messageError.innerHTML = left + " " + "More characters is required";
+        return false;
+    }
+    
+
+    messageError.innerHTML = '<i class="fa-solid fa-circle-check"></i>';
+    return true;
+}
+
+function validateForm() {
+    if(!validateName() || !validatePhone() || !validateEmail() || !validateMessage()) {
+       submitError.style.display = "block"
+       submitError.innerHTML = 'Please enter all details';
+       setTimeout(function(){submitError.style.display = 'none'}, 3000)
+       return false;
+    }
 }
